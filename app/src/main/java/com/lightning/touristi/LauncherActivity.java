449d17b1788e754c99d46f98c98bebb2e583d852
Siavash <s.kahe98@gmail.com>
2020-03-19 15:30:41 +0330

    login activity added

--- app/src/main/java/com/lightning/touristi/LauncherActivity.java
+++ app/src/main/java/com/lightning/touristi/LauncherActivity.java
@@ -2,7 +2,14 @@ package com.lightning.touristi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.lightning.touristi.ui.login.LoginActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        ImageView logo = findViewById(R.id.logo);
+
+        int splashTimeOut = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LauncherActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
     }
 }
